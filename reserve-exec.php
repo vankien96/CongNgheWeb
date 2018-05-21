0cf8c1e58684948b42b3d28b748e32304a69df79
vankien96 <vankien96@gmail.com>
2018-05-21 23:19:13 +0700

    fix order table and partyHall

--- reserve-exec.php
+++ reserve-exec.php
@@ -5,49 +5,41 @@
 	//Include database connection details
 	require_once('connection/config.php');
 	
-	
-	
-	//Function to sanitize values received from the form. Prevents SQL injection
-	function clean($str) {
-		$str = @trim($str);
-		if(get_magic_quotes_gpc()) {
-			$str = stripslashes($str);
+	if(isset($_SESSION['SESS_MEMBER_ID'])) {
+		$member_id = $_SESSION['SESS_MEMBER_ID'];
+		$check = false;
+		$partyhall_id = 0;
+	    $table_id = 0;
+	    $partyhall_flag = 0;
+	    $table_flag = 0;
+	    $date = "";
+	    $time = "";
+		if (isset($_POST['table']) && isset($_POST["date"]) && isset($_POST["time"])) {
+			$table_id = $_POST['table'];
+			$check = true;
+			$table_flag = 1;
+			$date = $_POST["date"];
+			$time = $_POST["time"];
 		}
-		return mysqli_real_escape_string($str);
-	}
-	
-	//Sanitize the POST values
-    $partyhall_id = 0;
-    $table_id = 0;
-    $partyhall_flag = 0;
-    $table_flag = 0;
-    
-    if(isset($_POST['table'])){
-        $table_id = clean($_POST['table']);
-        $table_flag = 1;
-    }
-    else if(isset($_POST['partyhall'])){
-        $partyhall_id = clean($_POST['partyhall']);
-        $partyhall_flag = 1;
-    }
-    
-	$date = clean($_POST['date']);
-	$time = clean($_POST['time']);
-	
-	//check if the id is set at the link
-	if (isset($_GET['id'])){
-	
-	    //get user id
-	    $id = $_GET['id'];
-	    
-	    //Create INSERT query
-	    $qry = "INSERT INTO reservations_details(member_id,table_id,partyhall_id,Reserve_Date,Reserve_Time,table_flag,partyhall_flag) VALUES('$id','$table_id','$partyhall_id','$date','$time','$table_flag','$partyhall_flag')";
-	    mysqli_query($db,$qry);
-	    
-	    //redirect to the reserve success page
-	    header("location: reserve-success.php");
-
-	}else {
-		die("Reservation failed! Please try again after a few minutes.");
+		if (isset($_POST['partyhall']) && isset($_POST["date"]) && isset($_POST["time"])) {
+			$partyhall_id = $_POST['partyhall'];
+			$partyhall_flag = 1;
+			$date = $_POST["date"];
+			$time = $_POST["time"];
+			$check = true;
+		}
+		if ($check) {
+			$qry = "INSERT INTO reservations_details(member_id,table_id,partyhall_id,Reserve_Date,Reserve_Time,table_flag,partyhall_flag) VALUES('$member_id','$table_id','$partyhall_id','$date','$time','$table_flag','$partyhall_flag')";
+			$result = mysqli_query($db,$qry);
+			if ($result) {
+				echo json_encode(["status" => true, "message" => ""]);
+			} else {
+				echo json_encode(["status" => false, "message" => "Đã có lỗi xảy ra. Xin vui lòng thử lại trong giây lát"]);
+			}
+		} else {
+			echo json_encode(["status" => false, "message" => "Đã có lỗi xảy ra. Xin vui lòng thử lại trong giây lát"]);
+		}
+	} else {
+		echo json_encode(["status" => false, "message" => "Đã có lỗi xảy ra. Xin vui lòng thử lại trong giây lát"]);
 	}
 ?>
--- tables.php
+++ tables.php
@@ -32,69 +32,133 @@ $memberId=$_SESSION['SESS_MEMBER_ID'];
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="swf/swfobject.js"></script>
   <link href="stylesheets/user_styles.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
-        <link type="text/css" href="stylesheets/bootstrap-timepicker.min.css" />
+  <link type="text/css" href="stylesheets/bootstrap-timepicker.min.css" />
   <script language="JavaScript" src="validation/user.js"></script>
   <script src="js/jquery-1.11.2.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-   <script type="text/javascript" src="js/bootstrap-timepicker.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap-timepicker.min.js"></script>\
+  <script type="text/javascript">
+    function makeTable() {
+      if ($('#orderForm')[0].checkValidity()) {
+        var tableId = document.getElementById("table").value;
+        var date = document.getElementById("date").value;
+        var time = document.getElementById("time").value;
+        $.ajax({
+            url: "reserve-exec.php",
+            method: "POST",
+            data: {
+              table: tableId,
+              date: date,
+              time: time
+            },
+            success: function(response) {
+              var dataRes = JSON.parse(response);
+              if (dataRes.status) {
+                alert("Đặt bàn thành công");
+                window.open("index.php","_self",false);
+              } else {
+                alert(dataRes.message);
+              }
+            }
+          })
+      } else {
+        alert("Vui lòng nhập toàn bộ các trường");
+      }
+    }
+  </script>
+
 </script>
 </head>
 <body>
 <div id="page">
- <nav class="navbar navbar-default">
+    <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
           <a class="navbar-brand" href="index.php">LK Restaurant</a>
         </div>
         <ul class="nav navbar-nav">
-          <li class="active"><a href="index.php">Trang chủ</a></li>
+          <li><a href="index.php">Trang chủ</a></li>
           <li><a href="foodzone.php">Khu ẩm thực</a></li>
-          <li><a href="specialdeals.php">Ưu đãi đặc biệt</a></li>
+          <li class="active"><a href="specialdeals.php">Ưu đãi đặc biệt</a></li>
           <li><a href="aboutus.php">Về chúng tôi</a></li>
         </ul>
       </div>
     </nav>
-<div id="header">
-  <div id="logo"> <a href="index.php" class="blockLink"></a></div>
-</div>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="images/1.jpg" alt="Los Angeles" height="150" width="1000">
+        </div>
+
+        <div class="item">
+          <img src="images/2.jpg" alt="Chicago" height="150" width="1000">
+        </div>
+
+        <div class="item">
+          <img src="images/3.jpg" alt="New York" height="150" width="1000">
+        </div>
+
+        <div class="item">
+          <img src="images/4.jpg" alt="New York" height="150" width="1000">
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
 <div id="center">
-<h1>RESERVE TABLE(S)</h1>
+<h1>ĐẶT BÀN</h1>
   <div style="border:#66FF99 solid 1px;padding:4px 6px 2px 6px">
 <a href="member-profile.php">Thông tin cá nhân</a> | <a href="cart.php">Đơn hàng[<?php echo $num_items;?>]</a> |  <a href="inbox.php">Tin nhắn[<?php echo $num_messages;?>]</a> | <a href="tables.php">Bàn</a> | <a href="partyhalls.php">Hội trường</a> | <a href="ratings.php">Đánh giá</a> | <a href="logout.php">Đăng xuất</a>
 <p>&nbsp;</p>
 <p>Ở đây bạn có thể đặt bạn ở nhà hàng của chúng tôi... Để biết thêm thông tin <a href="aboutus.php">Nhấn vào đây</a> để liên lạc với chúng tôi.
 <hr>
-<form name="tableForm" id="tableForm" method="post" action="reserve-exec.php?id=<?php echo $_SESSION['SESS_MEMBER_ID'];?>" onsubmit="return tableValidate(this)">
+<form id="orderForm">
     <table align="center" >
         <CAPTION><h2>ĐẶT BÀN</h2></CAPTION>
         <tr>
             <td class="navbar-brand">Bàn số/tên:</td>
-            <td><select name="table" id="table" class="form-control">
+            <td><select name="table" id="table" class="form-control" required>
             <option value="select">- chọn bàn -
             <?php 
             //loop through tables table rows
-            while ($row=mysqli_fetch_array($tables)){
-            echo "<option value=$row[table_id]>$row[table_name]"; 
-            }
+              while ($row=mysqli_fetch_array($tables)){
+                echo "<option value=$row[table_id]>$row[table_name]"; 
+              }
             ?>
             </select>
             </td>
         </tr>
         <tr>
             <td class="navbar-brand"> Ngày:</td>
-            <td><input type="date" name="date" id="date"  class="form-control"/></td></tr>
+            <td><input type="date" name="date" id="date"  class="form-control" required/></td></tr>
         <tr>
             <td class="navbar-brand">Thời gian:</td>
             <td>
-              <input type="time" name="time" id="time" class="form-control input-small" class="form-control"/>
+              <input type="time" name="time" id="time" class="form-control input-small" class="form-control" required/>
              </div>
             </td>
         </tr>
         <tr>
-            <td colspan="2" align="center"><input type="submit" value="Đặt"  class="btn btn-primary"></td>
+            <td colspan="2" align="center"><input type="button" value="Đặt" class="btn btn-primary" onclick="makeTable()"></td>
         </tr>
     </table>
 </form>
